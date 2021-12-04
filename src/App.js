f9943f995c227da1342947db57676cb017c38af8
Dipen505 <dipenchakma505@gmail.com>
2021-12-04 15:59:32 +0600

    pass object to components from an array and access the object

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ function App() {
     name: "Dipen",
     Job: "Singer"
   }
+  const products = [
+    {name: "Adobe", price: "$10", validiy: "1 month"},
+    {name: "Amazon", price: "$20", validiy: "3 month"},
+    {name: "Facebook", price: "$40", validiy: "6 month"},
+    {name: "Twitter", price: "$40", validiy: "7 month"}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-      <Person profile = {person}></Person>
-      <Person profile = {person}></Person>
-      <Person profile = {person}></Person>
-      <Person profile = {person}></Person>
+      <Person profile = {person}></Person>   
+      <Products detail = {products[0]}></Products>
+      <Products detail = {products[1]}></Products>
+      <Products detail = {products[2]}></Products>
+      <Products detail = {products[3]}></Products>
       </header>
     </div>
   );
 }
 function Person(props) {
-  console.log(props);
   const personStyle = {
     border: "1px solid blue",
     backgroundColor: "red",
@@ -33,7 +39,23 @@ function Person(props) {
     </div>
   )
 }
-
+function Products (props) {
+  const {name,price,validiy} = props.detail;
+  const productsStyle = {
+    border: "2px solid white",
+    borderRadius: "5px",
+    backgroundColor: "royalblue",
+    margin: "5px"
+  }
+  return (
+    <div style = {productsStyle}>
+      <h5>Product Name : {name} </h5>
+      <h5>Product Price : {price} </h5>
+      <p>Product Validity : {validiy} </p>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 
 
 export default App;
